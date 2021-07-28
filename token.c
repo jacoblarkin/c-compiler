6591ebc2d8dd7570ca0940549c3628058fb2c1cf
Jacob Larkin <larkin.jacob@gmail.com>
2021-07-28 15:19:12 -0400

    Add token files for lexer

---
+++ token.c
@@ -0,0 +1,56 @@
+#include "token.h"
+
+#include <stdlib.h>
+
+Token token_list_pop_back(TokenList* list)
+{
+  Token tok = {UNKNOWN, NULL};
+  if(list && list->last) {
+    tok = list->last->tok;
+    TokenListNode* new_back = list->last->prev;
+    new_back->next = NULL;
+    free(list->last);
+    list->last = new_back;
+  }
+  return tok;
+}
+
+Token token_list_pop_front(TokenList* list)
+{
+  Token tok = {UNKNOWN, NULL};
+  if(list && list->first) {
+    tok = list->first->tok;
+    TokenListNode* new_front = list->first->next;
+    new_front->prev = NULL;
+    free(list->first);
+    list->first = new_front;
+  }
+  return tok;
+}
+
+int token_list_push(TokenList* list, Token tok)
+{
+  if(!list) {
+    return 1;
+  }
+
+  TokenListNode* node = malloc(sizeof(TokenListNode));
+  if(!node) {
+   return 1; 
+  }
+
+  node->tok = tok;
+  node->prev = list->last;
+  node->next = NULL;
+
+  if(list->last) {
+    list->last->next = node;
+  }
+  if(!list->first) {
+    list->first = node;
+  }
+
+  list->last = node;
+
+  return 0;
+}
---
+++ token.h
@@ -0,0 +1,39 @@
+#ifndef __TOKEN_H__
+#define __TOKEN_H__
+
+typedef enum TokenType_t {
+  UNKNOWN = 0,
+  LEFT_BRACE,
+  RIGHT_BRACE,
+  LEFT_PAREN,
+  RIGHT_PAREN,
+  SEMICOLON,
+  INT,
+  RETURN,
+  IDENTIFIER,
+  INT_LITERAL,
+  HEX_LITERAL,
+  OCT_LITERAL
+} TokenType;
+
+typedef struct Token_t {
+  TokenType type;
+  char* value;
+} Token;
+
+typedef struct TokenListNode_t {
+  Token tok;
+  struct TokenListNode_t* prev;
+  struct TokenListNode_t* next;
+} TokenListNode;
+
+typedef struct TokenList_t {
+  TokenListNode* first;
+  TokenListNode* last;
+} TokenList;
+
+Token token_list_pop_back(TokenList*);
+Token token_list_pop_front(TokenList*);
+int token_list_push(TokenList*, Token);
+
+#endif
