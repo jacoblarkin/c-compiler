2d2fbc5f765a08943b92af15170e59f63b72ea3d
Jacob Larkin <larkin.jacob@gmail.com>
2021-10-19 10:46:33 -0400

    Generate asm for goto and labels

--- generator.c
+++ generator.c
@@ -20,12 +20,14 @@ void write_statement_assembly(StatementNode*, FILE*, int);
 void write_expression_assembly(Register, ExpressionNode*, FILE*);
 void check_next_reg(Register);
 int count_local_vars(BlockNode*);
+void construct_label_table(SymbolTable*, BlockNode*);
 size_t get_symbol_offset(char*, FILE*);
 
 int tag_counter = 0;
 
 SymbolTable* main_st;
 SymbolTable* top_st;
+SymbolTable* labels;
 
 int func_stack_offset;
 
@@ -37,6 +39,7 @@ void generate_assembly(ProgramNode prgm, const char* filename)
   assembly_filename = calloc(len+1, sizeof(char));
   strncpy(assembly_filename, filename, len);
   assembly_filename[len-1] = 's';
+  labels = NULL;
   
   FILE* as_file = fopen(assembly_filename, "w");
   if(!as_file) {
@@ -64,6 +67,12 @@ void write_ast_assembly(ProgramNode prgm, FILE* as_file)
     fprintf(as_file, "  sub sp, sp, #%i\n", func_stack_offset);
     int ret_tag = tag_counter++;
     top_st = NULL;
+    SymbolTable* label_st = malloc(sizeof(SymbolTable));
+    label_st->top = NULL;
+    label_st->next = labels;
+    labels = label_st;
+    push_constructed_symbol(NULL, 0, labels);
+    construct_label_table(labels, prgm.main->body);
     write_block_assembly(prgm.main->body, as_file, ret_tag);
     fprintf(as_file, ".L%i:\n", ret_tag);
     fprintf(as_file, "  add sp, sp, #%i\n", func_stack_offset);
@@ -118,6 +127,7 @@ void write_statement_assembly(StatementNode* stmt, FILE* as_file, int ret_tag)
   int tag0;
   int tag1;
   int tag2;
+  Symbol label;
   switch(stmt->type) {
   case RETURN_STATEMENT:
     write_expression_assembly(X0, stmt->expression, as_file); 
@@ -249,6 +259,28 @@ void write_statement_assembly(StatementNode* stmt, FILE* as_file, int ret_tag)
     break;
   case BLOCK_STATEMENT:
     write_block_assembly(stmt->block, as_file, ret_tag);
+    break;
+  case GOTO_STATEMENT:
+    label = find_symbol(stmt->label_name, labels);
+    if(!label.name) {
+      puts("Error: Could not find label for goto");
+      puts(stmt->label_name);
+      fclose(as_file);
+      remove(assembly_filename);
+      exit(1);
+    }
+    fprintf(as_file, "  b L%zu\n", label.address);
+    break;
+  case LABEL:
+    label = find_symbol(stmt->label_name, labels);
+    if(!label.name) {
+      puts("Error: Could not find label");
+      puts(stmt->label_name);
+      fclose(as_file);
+      remove(assembly_filename);
+      exit(1);
+    }
+    fprintf(as_file, ".L%zu:\n", label.address);
     break;
   case EXPRESSION:
     write_expression_assembly(X0, stmt->expression, as_file);
@@ -619,6 +651,40 @@ int count_local_vars(BlockNode* block)
     }
   }
   return local_vars;
+}
+
+void construct_label_table(SymbolTable* st, BlockNode* block)
+{
+  for(unsigned int i = 0; i < block->count; i++) {
+    if(block->body[i]->type == DECLARATION_ITEM) {
+      continue;
+    }
+    StatementNode* stmt = block->body[i]->stmt;
+    switch(stmt->type) {
+    case LABEL:
+      push_constructed_symbol(stmt->label_name, tag_counter++, st);
+      break;
+    case FORDECL_LOOP:
+    case WHILE_LOOP:
+    case DO_LOOP:
+    case FOR_LOOP:
+      if (stmt->loop_stmt->type == BLOCK_STATEMENT) {
+        construct_label_table(st, stmt->loop_stmt->block);
+      }
+      break;
+    case CONDITIONAL:
+      if (stmt->if_stmt->type == BLOCK_STATEMENT) {
+        construct_label_table(st, stmt->if_stmt->block);
+      }
+      if (stmt->else_stmt 
+          && stmt->else_stmt->type == BLOCK_STATEMENT) {
+        construct_label_table(st, stmt->else_stmt->block);
+      }
+      break;
+    default:
+      break;
+    }
+  }
 }
 
 size_t get_symbol_offset(char* name, FILE* as_file)
